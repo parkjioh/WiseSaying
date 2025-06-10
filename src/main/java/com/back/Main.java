4259f5f4b5d1ae971d606ab20fb2011d5ab6ad67
parkjioh <jioh035@kakao.com>
2025-06-10 17:45:04 +0900

    요구사항3단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //요구사항 1단계
+        
         System.out.println("==명언 앱 ==");
-
+        
         Scanner sc = new Scanner(System.in);
 
         while(true){
@@ -17,12 +17,16 @@ public class Main {
             if(cmd.equals("종료")){
                 break;
             }
-            //요구사항 2단계
+            
             if(cmd.equals("등록")){
                 System.out.print("명언 : ");
                 String wiseSayingContent = sc.nextLine().trim();
                 System.out.print("작가 : ");
                 String wiseSayingAuthor = sc.nextLine().trim();
+
+
+                System.out.println("1번 명언이 등록되었습니다.");
+                
             }
         }
 
