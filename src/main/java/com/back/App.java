2717f73336fa351559f0027e66ca71d6f7ba6a76
parkjioh <jioh035@kakao.com>
2025-06-11 14:43:29 +0900

    로직

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -34,10 +34,9 @@ public class App {
         System.out.println("번호 / 작가 / 명언 ");
         System.out.println("-------------------------");
 
-        for(int i=wiseSayingsLastIndex; i >= 0 ; i--){
-            WiseSaying wiseSaying = wiseSayings[i];
+        WiseSaying[] forListWiseSayings = findForList();
 
-
+        for (WiseSaying wiseSaying : forListWiseSayings) {
             System.out.printf("%d / %s / %s \n", wiseSaying.id, wiseSaying.author, wiseSaying.content);
         }
     }
@@ -48,14 +47,38 @@ public class App {
         System.out.print("작가 : ");
         String wiseSayingAuthor = sc.nextLine().trim();
 
-        int id = ++lastId;
 
-        WiseSaying wiseSaying = new WiseSaying(); // while문 안에 안 넣으면 계속 같은 객체만 덮어씀!!!
-        wiseSaying.id = id;
-        wiseSaying.content = wiseSayingContent;
-        wiseSaying.author = wiseSayingAuthor;
-        wiseSayings[++wiseSayingsLastIndex] = wiseSaying;
+        WiseSaying wiseSaying = write(wiseSayingContent, wiseSayingAuthor);
         System.out.println( "%d번 명언이 등록되었습니다.".formatted(wiseSaying.id));
 
     }
+
+    WiseSaying write(String content, String author){
+        WiseSaying wiseSaying = new WiseSaying(); // while문 안에 안 넣으면 계속 같은 객체만 덮어씀!!!
+        wiseSaying.id = ++lastId;
+        wiseSaying.content = content;
+        wiseSaying.author = author;
+
+        wiseSayings[++wiseSayingsLastIndex] = wiseSaying;
+
+        return wiseSaying;
+    }
+
+    int getSize() {
+        return wiseSayingsLastIndex + 1;
+    }
+
+    WiseSaying[] findForList() {
+        WiseSaying[] forListWiseSayings = new WiseSaying[getSize()];
+
+        int forListWiseSayingsIndex = -1;
+
+        for (int i=wiseSayingsLastIndex; i>=0;i--){
+            forListWiseSayings[++forListWiseSayingsIndex] = wiseSayings[i];
+        }
+
+        return forListWiseSayings;
+    }
+
 }
+
