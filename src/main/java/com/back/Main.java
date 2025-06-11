d1032566aa78dc3050ac34dc6a75e70a91b4e6c2
parkjioh <jioh035@kakao.com>
2025-06-11 11:18:28 +0900

    요구사항5단계1

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -9,12 +9,14 @@ public class Main {
         
         Scanner sc = new Scanner(System.in);
         int lastId = 0;
-
+        WiseSaying wiseSaying = null;
 
         while(true){
 
             System.out.print("명령) ");
             String cmd = sc.nextLine().trim();
+
+
 
             if(cmd.equals("종료")){
                 break;
@@ -26,13 +28,22 @@ public class Main {
 
                 int id = ++lastId;
 
-                WiseSaying wiseSaying = new WiseSaying();
+                wiseSaying = new WiseSaying(); // while문 안에 안 넣으면 계속 같은 객체만 덮어씀!!! 
                 wiseSaying.id = id;
                 wiseSaying.content = wiseSayingContent;
                 wiseSaying.author = wiseSayingAuthor;
 
                 System.out.println( "%d번 명언이 등록되었습니다.".formatted(wiseSaying.id));
                 
+            }
+            else if(cmd.equals("목록")){
+
+                if(wiseSaying == null) continue;
+
+                System.out.println("번호 / 작가 / 명언 ");
+                System.out.println("-------------------------");
+                System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
+
             }
         }
 
